c274f67fc08e5c6f016de410a09e77db4ca3469e
olayinkaajayi <work.for.funmilade@gmail.com>
2024-04-15 17:54:26 +0100

    corrected a syntax error in the signature of agcn.

--- model/agcn.py
+++ model/agcn.py
@@ -133,7 +133,6 @@ class TCN_GCN_unit(nn.Module):
 class Model(nn.Module):
     def __init__(self, num_class=60, num_point=25, num_person=2, graph=None,
                     graph_args=dict(), in_channels=3, d=8, PE_name='', use_PE=True, just_project=False):
-                    graph_args=dict(), in_channels=3, d=8, PE_name='', use_PE=True, just_project=False):
         super(Model, self).__init__()
 
         if graph is None:
